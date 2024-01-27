66421bac1e080fb19cc2f64cc78258ae59a5f9f4
Krsto Kostic <kostic.krsto@gmail.com>
2024-01-27 16:29:43 +0100

    Creating user modal

---
+++ backend/main_service/src/modals/user.modal.ts
@@ -0,0 +1,9 @@
+class User {
+  constructor(
+    public readonly firstName: string,
+    public readonly lastName: string,
+    public readonly username: string,
+    public readonly email: string,
+    public readonly password: string
+  ) {}
+}
